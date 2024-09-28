c923b82c4b87a989cc6007419a895deabf8598b9
Neil de Carteret <git@lumphammer.net>
2024-09-28 19:09:30 +0100

    more work on theme

--- src/toc2eQuickstartTheme.tsx
+++ src/toc2eQuickstartTheme.tsx
@@ -2,28 +2,22 @@ import { SeedColorsV1, ThemeSeedV1 } from "@lumphammer/investigator-fvtt-types";
 
 import { averiaLibre, moduleId } from "./constants";
 
-// const assetPath = getRoute("/modules/trail-of-cthulhu-2e/assets");
-
-const lightBackground = "#fafafa";
-const darkBackground = "#e1e1e1";
-
-// const glint = "#f3eee1";
-// const offGlint = "#efe8d6";
-// const outsetBorderColors = `${glint} ${offGlint} ${offGlint} ${glint}`;
-
 const colors: SeedColorsV1 = {
   accent: "#a8821a",
-  accentContrast: "#404",
+  accentContrast: "#fff",
   glow: "#ecd597",
   wallpaper: "#f2f1ef",
-  backgroundPrimary: `#ffffff99`,
-  backgroundSecondary: `#f2f1efdd`,
+  backgroundPrimary: "#ffffff99",
+  backgroundSecondary: "#f2f1efdd",
   backgroundButton: "#90752c44",
   text: "#333",
   controlBorder: "#666",
+  danger: "#b03",
 };
 
-const starfieldImage = `url(modules/${moduleId}/assets/toc2eQuickstartTheme/ToC_character_sheet_background.webp)`;
+const borderColor = "#cfa225";
+
+const wallpaperImage = `url(modules/${moduleId}/assets/toc2eQuickstartTheme/ToC_character_sheet_background.webp)`;
 
 export const toc2eQuickstartThemeSeed: ThemeSeedV1 = {
   schemaVersion: "v1",
@@ -33,24 +27,9 @@ export const toc2eQuickstartThemeSeed: ThemeSeedV1 = {
     ${averiaLibre.importStatement}
   `,
   largeSheetRootStyle: {
-    backgroundImage: starfieldImage,
+    backgroundImage: wallpaperImage,
   },
-  smallSheetRootStyle: {
-    // background: `linear-gradient(to bottom, ${darkBackground}dd 0%, ${darkBackground}55 100%), ${starfieldImage}`,
-    // background: starfieldImage,
-    // ":before": {
-    //   content: "''",
-    //   position: "absolute",
-    //   top: "0",
-    //   left: "0",
-    //   width: "100%",
-    //   height: "100%",
-    //   backgroundColor: "#0003",
-    //   // zIndex: "-1",
-    //   backdropFilter: "blur(20px)",
-    // },
-    // color: lightBackground,
-  },
+  smallSheetRootStyle: {},
   bodyFont: averiaLibre.fontFamily,
   displayFont: "normal normal 1em 'Engebrechtre', serif",
   logo: {
@@ -82,42 +61,52 @@ export const toc2eQuickstartThemeSeed: ThemeSeedV1 = {
       // mixBlendMode: "overlay",
     },
   },
-  // panelStylePrimary: {
-  //   backgroundColor: "#0000",
-  //   backdropFilter: "blur(15px)",
-  //   borderStyle: "solid",
-  //   borderColor: "#e9e9",
-  //   borderWidth: "1px",
-  //   color: colors.text,
-  // },
-  // panelStyleSecondary: {
-  //   backgroundColor: "#0000",
-  //   backdropFilter: "blur(5px)",
-  //   borderStyle: "solid",
-  //   borderColor: "#e9e9",
-  //   borderWidth: "1px",
-  //   color: colors.text,
-  // },
-  // tabContentStyle: {
-  //   backgroundColor: "#1111",
-  //   backdropFilter: "blur(20px)",
-  //   borderStyle: "none solid solid solid",
-  //   borderColor: "#e9e9",
-  //   borderWidth: "1px",
-  //   color: colors.text,
-  // },
-  // tabStyle: {
-  //   padding: "0.5em",
-  //   flex: "1",
-  //   // justifySelf: "center",
-  //   // alignSelf: "center",
-  //   textAlign: "center",
-  //   fontSize: "1.1em",
-  //   background: `linear-gradient(0deg, ${darkBackground}77 0%, ${darkBackground}77 0em, ${colors.backgroundSecondary} 0.4em, ${colors.backgroundSecondary} 100%)`,
-  // },
-  // tabActiveStyle: {
-  //   background: `linear-gradient(0deg, ${colors.backgroundPrimary} 0%,  ${lightBackground} 100%)`,
-  // },
+  panelStylePrimary: {
+    backgroundColor: "#fff3",
+    borderStyle: "double",
+    borderColor,
+    borderWidth: "3px",
+  },
+  panelStyleSecondary: {
+    backgroundColor: "#fff3",
+    borderStyle: "solid",
+    borderColor,
+    borderWidth: "1px",
+  },
+  tabContentStyle: {
+    backgroundColor: colors.backgroundPrimary,
+    // backdropFilter: "blur(20px)",
+    borderStyle: "none double double double",
+    borderColor,
+    borderWidth: "3px",
+    color: colors.text,
+  },
+  tabStyle: {
+    padding: "0.3em 0",
+    color: colors.accent,
+    flex: "1",
+    textAlign: "center",
+    fontSize: "1.5em",
+    borderStyle: "double",
+    borderColor,
+    borderWidth: "3px",
+    background: `
+      linear-gradient(
+        0deg,
+        ${colors.backgroundPrimary} 0%,
+        ${colors.backgroundPrimary} 100%
+      )
+    `,
+  },
+  tabActiveStyle: {
+    borderStyle: "double double none double",
+  },
+  tabSpacerStyle: {
+    borderStyle: "none none double none",
+    width: "0.5rem",
+    borderColor,
+    borderWidth: "3px",
+  },
   // panelStyleSecondary: {
   //   backgroundColor: colors.backgroundSecondary,
   //   borderStyle: "solid",
